53d5a0e1b4aa29788c1d1a458a5fe74e09b03451
ViniciusPaixaoSilva <viniciuspaixao001@gmail.com>
2021-03-19 13:26:15 -0300

    Mais alteração nas classes

--- Empregado.java
+++ Empregado.java
@@ -37,10 +37,34 @@ public class Empregado {
   }
 
   public void setTipo(int tipo) {
-    this.tipo = tipo; 
+    this.tipo = tipo;
   }
 
   public int tipo() {
     return this.tipo;
+  }
+
+  public void setSalario(double salario) {
+    this.salario = salario;
+  }
+
+  public double getSalario() {
+    return this.salario;
+  }
+
+  public void setComissao(double comissao) {
+    this.comissao = comissao;
+  }
+
+  public double getComissao() {
+    return this.comissao;
+  }
+
+  public void setBonus(double bonus) {
+    this.bonus = bonus;
+  }
+
+  public double getBonus() {
+    return this.bonus;
   }
 }
